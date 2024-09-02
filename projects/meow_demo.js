7901605d0cbbe738d36df045a3f08091acf47a5d
Zac Stewart <git@zacstewart.com>
2024-09-02 18:40:33 +0800

    Remove projects pages

--- projects/meow_demo.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('#example-button').click(function () {
-    $.meow({
-      message: $('#example-input'),
-      title: 'Hello, World',
-      icon: 'http://zacstewart.com/Meow/nyan-cat.gif'
-    });
-  });
-});
